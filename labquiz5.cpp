eaa7ee530302ad43997c518ed9263a28f548ae07
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2023-03-06 17:43:25 -0500

    Create labquiz5.cpp

---
+++ labquiz5.cpp
@@ -0,0 +1,25 @@
+
+/*
+Frida Mesa 
+03/06/23
+Lab quiz 5
+ */
+ 
+#include <iostream>
+using namespace std;
+
+int main() {
+   
+for(int i = 0; i < 6; i++){ 
+
+for(int j = 0; j < 5; j++){ 
+cout << "+";
+
+}
+
+cout << endl;
+}
+
+return 0;
+
+}
